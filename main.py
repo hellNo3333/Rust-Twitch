50f39f80428eede7e9bb9174c9de886da2180e26
hellNo <hellnoromania@gmail.com>
2022-05-06 17:35:40 +0300

    added options
    --headless --mute-audio

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
 from API import API
 import sys
 import os
 
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--mute-audio")
+
 def main():
 	res = False
-	driver = webdriver.Chrome("driver" + os.path.sep + "chromedriver")
+	driver = webdriver.Chrome("driver" + os.path.sep + "chromedriver", options=options)
 	Api = API(driver)
 	driver.get("https://www.twitch.tv/")
 
@@ -35,5 +40,3 @@ def main():
 			main()
 
 main()
-
-
